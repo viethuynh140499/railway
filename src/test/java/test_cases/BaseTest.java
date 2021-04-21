0de729cf551444dcbaf392bd38c1cba3ec05a98e
Your Name <vietpro.huynhh@gmail.com>
2021-04-21 23:06:13 +0700

    login with json data

--- src/test/java/test_cases/BaseTest.java
+++ src/test/java/test_cases/BaseTest.java
@@ -2,38 +2,35 @@ package test_cases;
 
 import common.constant.Constant;
 import common.helper.Helper;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     JavascriptExecutor js;
     ChromeOptions chromeOptions;
 
-    @BeforeTest
+    @BeforeMethod
     public void beforeTest() throws IOException {
-        System.setProperty("webdriver.chrome.driver", Helper.getProjectPath() + "/src/main/resources/Executables/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", Helper.getProjectPath() + "/src/main/resources/Executables/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", Helper.getProjectPath() + "/src/main/resources/Executables/chromedriver");
         chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         Constant.WEBDRIVER = new ChromeDriver(chromeOptions);
         Constant.WEBDRIVER.manage().window().maximize();
-        Constant.WEBDRIVER.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        Constant.WEBDRIVER.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         js = (JavascriptExecutor) Constant.WEBDRIVER;
+
     }
-    @AfterTest
-    public void afterTest() throws  IOException{
+
+    @AfterMethod
+    public void afterTest() throws IOException {
         System.out.println("Post-condition");
         Constant.WEBDRIVER.quit();
     }
